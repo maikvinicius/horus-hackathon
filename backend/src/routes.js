a6900defea972c81eaefb10f3fdef4a621c5270d
lucasvma <venturaml21@gmail.com>
2020-04-22 20:27:27 -0300

    Return random sex and random age between 13 yo and 73 yo on the /random-geo-coordinates; Enable CORS on API

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,6 +5,12 @@ const config = require('./config/mongoose');
 const GeneratePeopleController = require('./controllers/GeneratePeople');
 const RandomGeoCoordinatesController = require('./controllers/RandomGeoCoordinatesController');
 
+routes.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 routes.post('/generate-people', GeneratePeopleController.generatePeople);
 routes.post('/random-geo-coordinates', RandomGeoCoordinatesController.generateRandomGeoCoordinates);
 
