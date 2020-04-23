f6088d02006c6698d9f6b5c2b2ccba03cfa2eea4
lucasvma <venturaml21@gmail.com>
2020-04-23 01:12:14 -0300

    Fix file for CORS code

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,16 @@ const app = express();
 
 const server = require('http').Server(app);
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
+app.use(cors({ credentials: true, origin: true }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(require("./routes"));
