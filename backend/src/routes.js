8ce6796fa8e4d18fca41f51566a3d18414573fb3
lucasvma <venturaml21@gmail.com>
2020-04-23 00:45:20 -0300

    Fixing CORS on browsers

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -8,6 +8,33 @@ const RandomGeoCoordinatesController = require('./controllers/RandomGeoCoordinat
 
 routes.use(cors({ credentials: true, origin: true }));
 
+routes.all('*', function(req, res,next) {
+    /**
+     * Response settings
+     * @type {Object}
+     */
+    var responseSettings = {
+        "AccessControlAllowOrigin": req.headers.origin,
+        "AccessControlAllowHeaders": "Content-Type,X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5,  Date, X-Api-Version, X-File-Name",
+        "AccessControlAllowMethods": "POST, GET, PUT, DELETE, OPTIONS",
+        "AccessControlAllowCredentials": true
+    };
+
+    /**
+     * Headers
+     */
+    res.header("Access-Control-Allow-Credentials", responseSettings.AccessControlAllowCredentials);
+    res.header("Access-Control-Allow-Origin",  responseSettings.AccessControlAllowOrigin);
+    res.header("Access-Control-Allow-Headers", (req.headers['access-control-request-headers']) ? req.headers['access-control-request-headers'] : "x-requested-with");
+    res.header("Access-Control-Allow-Methods", (req.headers['access-control-request-method']) ? req.headers['access-control-request-method'] : responseSettings.AccessControlAllowMethods);
+
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    } else {
+        next();
+    }
+});
+
 routes.post('/generate-people', GeneratePeopleController.generatePeople);
 routes.post('/random-geo-coordinates', RandomGeoCoordinatesController.generateRandomGeoCoordinates);
 
