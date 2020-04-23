bb2b2f4f3b6c62371190e68b9f05e250043bc8a4
lucasvma <venturaml21@gmail.com>
2020-04-23 01:12:14 -0300

    Fix file for CORS code

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -6,16 +6,6 @@ const cors = require('cors');
 const GeneratePeopleController = require('./controllers/GeneratePeople');
 const RandomGeoCoordinatesController = require('./controllers/RandomGeoCoordinatesController');
 
-express.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3333');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
-  
-express.use(cors({ credentials: true, origin: true }));
-
 routes.post('/generate-people', GeneratePeopleController.generatePeople);
 routes.post('/random-geo-coordinates', RandomGeoCoordinatesController.generateRandomGeoCoordinates);
 
